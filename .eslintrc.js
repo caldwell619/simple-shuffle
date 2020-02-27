954c17718c1e9e63128839a65154aed5969a24f7
caldwell619 <caldwell8814@gmail.com>
2020-02-27 09:03:58 -0600

    adding module to lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,23 @@
-const isRunningLocally = process.env.NODE_ENV !== 'production'
+const isRunningLocally = process.env.NODE_ENV !== "production";
 
 module.exports = {
-	root: true,
-	env: {
+  root: true,
+  env: {
     node: true,
     es6: true
-	},
-	extends: ['eslint:recommended'],
-	rules: {
-    'no-console': isRunningLocally ? 'off' : 'error',
-    // always use single quotes
-    quotes: [2, 'single', { avoidEscape: true }],
-    // errors when semi colon are used to close
-    semi: ['error', 'never'],
-    // requires commas after every available option
-    'comma-dangle': [0, 'always']
   },
-	parserOptions: {
-    ecmaVersion: 10
-	}
-}
+  extends: ["eslint:recommended"],
+  rules: {
+    "no-console": isRunningLocally ? "off" : "error",
+    // always use single quotes
+    quotes: [2, "single", { avoidEscape: true }],
+    // errors when semi colon are used to close
+    semi: ["error", "never"],
+    // requires commas after every available option
+    "comma-dangle": [0, "always"]
+  },
+  parserOptions: {
+    ecmaVersion: 10,
+    sourceType: "module",
+  }
+};
